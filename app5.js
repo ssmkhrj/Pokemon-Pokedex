8b321b763f910cae93eae481dd8aa5c029aed799
ssmkhrj <iamssmkhrj@gmail.com>
2020-09-19 21:03:51 +0530

    Added Generations

--- app5.js
+++ app5.js
@@ -1,4 +1,16 @@
 const pokemonContainer = document.querySelector(".pokemon-container");
+const buttons = document.querySelectorAll(".button-container button");
+const heading = document.querySelector("#heading");
+
+const generations = [
+  ["Kanto", 1, 151],
+  ["Johto", 152, 251],
+  ["Hoenn", 252, 386],
+  ["Sinnoh", 387, 483],
+  ["Unova", 494, 649],
+];
+
+let currentGen = 0;
 
 const colors = {
   grass: "#d2f2c2",
@@ -31,12 +43,16 @@ searchBoxContainer.innerHTML = `
 // This uses only a single fetch but the results will be shown only when all pokemons are fetched
 // ===============================================================================================
 
-const pokemonStartID = 1;
-const pokemonEndID = 150;
+async function getPokemons(pokemonStartID, pokemonEndID) {
+  pokemonContainer.innerHTML = `<span class="loader"></span>`;
 
-const promises = [];
+  // Restricting Clicking on Buttons
+  buttons.forEach((el) => {
+    console.log("IN");
+    el.classList.add("restrict-click");
+  });
 
-async function getPokemons() {
+  const promises = [];
   for (let id = pokemonStartID; id <= pokemonEndID; id++) {
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
     const json = await response.json();
@@ -56,7 +72,7 @@ async function getPokemons() {
 		<div class="pokemon-card">
 			<img src=${imageURL}></img>
 			<div class="circle"></div>
-			<h5 class="poke-id"> #${ind + 1} </h5>
+			<h5 class="poke-id"> #${el.id} </h5>
 			<h5 class="poke-name"> ${pokemonName.replace(/\w/, (ch) =>
         ch.toUpperCase()
       )} </h5>
@@ -77,9 +93,17 @@ async function getPokemons() {
     } else {
       pokemonCard.style.background = colors[pokemonTypes[0]];
     }
-
-    instantiateListener();
   });
+
+  // Enabling Clicking on Buttons
+  setTimeout(() => {
+    buttons.forEach((el) => {
+      console.log("OUT");
+      el.classList.remove("restrict-click");
+    });
+  }, 500);
+
+  instantiateListener();
 }
 
 function instantiateListener() {
@@ -100,4 +124,24 @@ function instantiateListener() {
   });
 }
 
-getPokemons();
+buttons.forEach((el) => {
+  el.addEventListener("click", (e) => {
+    if (
+      e.target.id === "right-btn" &&
+      !e.target.classList.contains("restrict-click")
+    ) {
+      currentGen = (currentGen + 1) % generations.length;
+      heading.innerText = generations[currentGen][0] + " Pokédex";
+      getPokemons(generations[currentGen][1], generations[currentGen][2]);
+    } else if (
+      e.target.id === "left-btn" &&
+      !e.target.classList.contains("restrict-click")
+    ) {
+      currentGen = (currentGen - 1 + generations.length) % generations.length;
+      heading.innerText = generations[currentGen][0] + " Pokédex";
+      getPokemons(generations[currentGen][1], generations[currentGen][2]);
+    }
+  });
+});
+
+getPokemons(generations[0][1], generations[0][2]);
