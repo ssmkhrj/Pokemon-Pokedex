d3dc8703e7e4504b9c100f7cc7a909812fba1a23
ssmkhrj <iamssmkhrj@gmail.com>
2020-09-19 14:23:28 +0530

    Updated Colors and Background

--- app5_new.js
+++ app5_new.js
@@ -1,24 +1,78 @@
 const pokemonContainer = document.querySelector(".pokemon-container");
 
 const colors = {
-  grass: "#A7DB8D7D",
-  poison: "#C183C17D",
-  fire: "#F5AC787D",
-  flying: "#C6B7F57D",
-  water: "#C6B7F57D",
-  bug: "#C6D16E7D",
-  normal: "#C6C6A77D",
-  electric: "#FAE0787D",
-  ground: "#EBD69D7D",
-  fighting: "#D678737D",
-  psychic: "#FA92B27D",
-  rock: "#D1C17D7D",
-  fairy: "#F4BDC97D",
-  steel: "#D1D1E07D",
-  ice: "#BCE6E67D",
-  ghost: "#A292BC7D",
-  dragon: "#A27DFA7D",
+  grass: "#d2f2c2",
+  poison: "#f7cdf7",
+  fire: "#ffd1b5",
+  flying: "#eae3ff",
+  water: "#c2f3ff",
+  bug: "#e0e8a2",
+  normal: "#e6e6c3",
+  electric: "#fff1ba",
+  ground: "#e0ccb1",
+  fighting: "#fcada9",
+  psychic: "#ffc9da",
+  rock: "#f0e09c",
+  fairy: "#ffdee5",
+  steel: "#e6eaf0",
+  ice: "#e8feff",
+  ghost: "#dbbaff",
+  dragon: "#c4bdff",
 };
+
+fetch("https://pokeapi.co/api/v2/pokemon?limit=150")
+  .then((response) => response.json())
+  .then((json) => {
+    let allPromises = [];
+    json.results.forEach((el) => {
+      allPromises.push(fetch(el.url));
+    });
+
+    return Promise.all(allPromises);
+  })
+  .then((responses) => {
+    let allJsons = [];
+    responses.forEach((el) => {
+      allJsons.push(el.json());
+    });
+
+    return Promise.all(allJsons);
+  })
+  .then((jsons) => {
+    pokemonContainer.innerHTML = "";
+    jsons.forEach((json, index) => {
+      const pokemonName = json.name;
+      const pokemonTypes = [];
+      json.types.forEach((item) => {
+        pokemonTypes.push(item.type.name);
+      });
+      pokemonContainer.innerHTML += `
+			<div class="pokemon-card">
+				<img src="https://pokeres.bastionbot.org/images/pokemon/${index + 1}.png"></img>
+				<div class="circle"></div>
+				<h5 class="poke-id"> #${index + 1} </h5>
+				<h5 class="poke-name"> ${pokemonName.replace(/\w/, (ch) =>
+          ch.toUpperCase()
+        )} </h5>
+				<h5> ${pokemonTypes
+          .join(" / ")
+          .replace(/\b\w/g, (ch) => ch.toUpperCase())} </h5>
+			</div>
+			`;
+      const pokemonCards = document.querySelectorAll(".pokemon-card");
+      const pokemonCard = pokemonCards[pokemonCards.length - 1];
+      if (pokemonTypes[1]) {
+        pokemonCard.style.background =
+          "linear-gradient(150deg," +
+          colors[json.types[0].type.name] +
+          " 50%," +
+          colors[json.types[1].type.name] +
+          " 50%)";
+      } else {
+        pokemonCard.style.background = colors[pokemonTypes[0]];
+      }
+    });
+  });
 
 const searchBoxContainer = document.createElement("div");
 searchBoxContainer.setAttribute("class", "search-box-container");
@@ -54,7 +108,7 @@ async function getPokemons() {
     const imageURL = el.sprites.other["official-artwork"].front_default;
     pokemonContainer.innerHTML += `
 		<div class="pokemon-card">
-			<img src=${imageURL}></img>
+			<img src="https://pokeres.bastionbot.org/images/pokemon/${ind + 1}.png"></img>
 			<div class="circle"></div>
 			<h5 class="poke-id"> #${ind + 1} </h5>
 			<h5 class="poke-name"> ${pokemonName.replace(/\w/, (ch) =>
